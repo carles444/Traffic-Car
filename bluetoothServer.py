12a9631dc7eb7b762620d7adb6f030e66ae84c56
carles444 <carlesandreu4@gmail.com>
2022-05-02 19:49:52 +0200

    initial version

--- bluetoothServer.py
+++ bluetoothServer.py
@@ -19,7 +19,7 @@ if __name__ == '__main__':
     os.system('hcitool dev | grep -o "[[:xdigit:]:]\{11,17\}" > temp/address.txt')
     with open('temp/address.txt', 'r') as file:
         MY_ADDRESS = file.readline()
-    MY_ADDRESS = MY_ADDRESS.replace('\n', '').lower()
+    MY_ADDRESS = MY_ADDRESS.replace('\n', '')
     PORT = 4
     sock = socket.socket(socket.AF_BLUETOOTH, socket.SOCK_STREAM, socket.BTPROTO_RFCOMM)
     sock.bind((MY_ADDRESS, PORT))
