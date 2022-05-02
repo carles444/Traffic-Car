aaa2cf0e9b3b1e2da0f1eaf869aac3fb875ffd01
carles444 <carlesandreu4@gmail.com>
2022-05-02 19:35:46 +0200

    initial version

--- bluetoothServer.py
+++ bluetoothServer.py
@@ -17,7 +17,7 @@ def remove_temp_files():
 if __name__ == '__main__':
     create_temp_files()
     os.system('hcitool dev | grep -o "[[:xdigit:]:]\{11,17\}" > temp/address.txt')
-    with open('/temp/address.txt', 'r') as file:
+    with open('temp/address.txt', 'r') as file:
         MY_ADDRESS = file.readline()
     MY_ADDRESS = MY_ADDRESS.replace('\n', '')
     PORT = 4
