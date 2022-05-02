d844e56fbc91ab5dd8f410323fbe2c9e7f9aa4f4
carles444 <carlesandreu4@gmail.com>
2022-05-02 19:47:53 +0200

    initial version

--- bluetoothClient.py
+++ bluetoothClient.py
@@ -19,6 +19,6 @@ def remove_temp_files():
 
 if __name__ == '__main__':
     sock = socket.socket(socket.AF_BLUETOOTH, socket.SOCK_STREAM, socket.BTPROTO_RFCOMM)
-    sock.connect((SERVER_ADDRESS, SERVER_PORT))
+    sock.connect((SERVER_ADDRESS.lower(), SERVER_PORT))
     data = sock.recv(1024)
     print(data)
--- bluetoothServer.py
+++ bluetoothServer.py
@@ -19,13 +19,15 @@ if __name__ == '__main__':
     os.system('hcitool dev | grep -o "[[:xdigit:]:]\{11,17\}" > temp/address.txt')
     with open('temp/address.txt', 'r') as file:
         MY_ADDRESS = file.readline()
-    MY_ADDRESS = MY_ADDRESS.replace('\n', '')
+    MY_ADDRESS = MY_ADDRESS.replace('\n', '').lower()
     PORT = 4
     sock = socket.socket(socket.AF_BLUETOOTH, socket.SOCK_STREAM, socket.BTPROTO_RFCOMM)
     sock.bind((MY_ADDRESS, PORT))
     sock.listen(1)
+    print('listening')
     while True:
         c, addr = sock.accept()
+        print('new connection')
         c.send('Connected'.encode())
         c.close()
         print(addr)
