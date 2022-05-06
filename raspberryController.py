82fdfcff28e5c642fc7f3a15f394b83cc5356996
root <carlesandreu4@gmail.com>
2022-05-06 20:43:23 +0200

    pybluez

--- raspberryController.py
+++ raspberryController.py
@@ -4,11 +4,12 @@ import shutil
 import time
 from manualController import manualDriver
 from logger import *
+import bluetooth
 
 RP_ADDR = 'DC:A6:32:AA:AA:E6'
 SERVER_ADDRESS = '04:6C:59:F1:F3:E1'
-SERVER_PORT = '00000000000000000000000000000005'
-
+SERVER_PORT = 5
+UUID = '00000000-0000-0000-0000-000000000005'
 
 def create_temp_files():
     if os.path.exists('temp'):
@@ -22,16 +23,15 @@ def remove_temp_files():
 
 
 class Controller:
-    def __init__(self, server_addr, port):
+    def __init__(self, uuid_service):
         self.modes = ['manual', 'autonomous', 'exit']
+        self.uuid_service = uuid_service
         self.connected = False
         self.DEFAULT_TIMEOUT = 5
-        self.logger = Logger().getLogger('controller', logging.DEBUG)
-        self.server_addr = server_addr
-        self.port = port
-        self.logger.debug(f'Controller initializated with Server addr: {self.server_addr}')
-        self.logger.debug(f'Controller initializated with port: {self.port}')
-        
+        self.logger = Logger().getLogger('Raspberry Controller', logging.DEBUG)
+        self.logger.debug(f'Controller initializated with uuid: {self.uuid_service}')
+       
+    """ 
     def connect(self):
         self.sock = socket.socket(socket.AF_BLUETOOTH, socket.SOCK_STREAM, socket.BTPROTO_RFCOMM)
         self.sock.settimeout(self.DEFAULT_TIMEOUT)
@@ -45,7 +45,19 @@ class Controller:
                 time.sleep(self.DEFAULT_TIMEOUT)
                 
         self.logger.info('Connected to controller successfully')
-    
+    """
+    def connect(self):
+        services = []
+        while len(services) == 0:
+            self.logger.info('Connecting to controller...')
+            services = bluetooth.find_service(uuid=self.uuid_service)
+        self.port = services[0]['port']
+        self.name = services[0]['name']
+        self.address = services[0]['host']
+        self.sock = bluetooth.BluetoothSocket(bluetooth.RFCOMM)
+        self.sock.connect((self.address, self.port))
+        self.logger.info('Connected to controller successfully')
+        
     def manual_mode(self):
         self.logger.info('Entering in manual mode')
         if self.manual_driver is None:
@@ -73,5 +85,5 @@ class Controller:
                 
 
 if __name__ == '__main__':
-    controller = Controller(SERVER_ADDRESS, SERVER_PORT)
+    controller = Controller(UUID)
     controller()
