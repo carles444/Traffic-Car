0f6856d392ef5c66f11476dcb78679fd406733ca
root <carlesandreu4@gmail.com>
2022-05-16 18:20:11 +0200

    component controlls

--- src/manualDriver.py
+++ src/manualDriver.py
@@ -1,6 +1,12 @@
-#import RPi.GPIO as gpio
+import RPi.GPIO as gpio
 from enum import IntEnum
 from logger import *
+from gpiozero import Servo
+
+class Pins(IntEnum):
+    DC_0 = 16
+    DC_1 = 18
+    SERVO = 22
 
 class Packet(IntEnum):
     SET_MODE = 1
@@ -12,27 +18,44 @@ class RobotState(IntEnum):
     EXIT = 3
 
 class MovementState(IntEnum):
-    FORWARD = 1
-    RIGHT = 8
-    BREAKS = 2
-    LEFT = 4
+    FORWARD = 0
+    RIGHT = 3
+    BREAKS = 1
+    LEFT = 2
     
 
 class manualDriver:
     def __init__(self, comunication_socket):
         self.communication_socket = comunication_socket
         self.logger = Logger().getLogger('Manual Driver', logging.DEBUG)
-        #gpio.setmode(gpio.BOARD)
+        gpio.setmode(gpio.BOARD)
+        gpio.setup(Pins.DC_0, gpio.OUT)
+        gpio.setup(Pins.DC_1, gpio.OUT)
+        self.servo = Servo(Pins.SERVO)
     
     def apply_movement(self, forward_bit, breaks_bit, left_bit, right_bit):
-        if forward_bit:
+        if forward_bit >> MovementState.FORWARD:
             self.logger.debug('forward')
-        if breaks_bit >> 1:
+            gpio.output(Pins.DC_0, True)
+            gpio.output(Pins.DC_0, True)
+        elif breaks_bit >> MovementState.BREAKS:
             self.logger.debug('breaks')
-        if left_bit >> 2:
+            gpio.output(Pins.DC_0, False)
+            gpio.output(Pins.DC_0, True)
+        else:
+            self.logger.debug('rest power')
+            gpio.output(Pins.DC_0, False)
+            gpio.output(Pins.DC_0, False)
+            
+        if left_bit >> MovementState.LEFT:
             self.logger.debug('left')
-        if right_bit >> 3:
+            self.servo.min()
+        elif right_bit >> MovementState.RIGHT:
             self.logger.debug('right')
+            self.servo.max()
+        else:
+            self.logger.debug('rest steering')
+            self.servo.mid()
             
     def __call__(self):
         while True:
