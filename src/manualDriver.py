199d5884156bf94bf8597d80be6d7af16eeaeaa8
root <carlesandreu4@gmail.com>
2022-05-17 16:33:43 +0200

    t

--- src/manualDriver.py
+++ src/manualDriver.py
@@ -31,7 +31,7 @@ class manualDriver:
         gpio.setmode(gpio.BOARD)
         gpio.setup(Pins.DC_0, gpio.OUT)
         gpio.setup(Pins.DC_1, gpio.OUT)
-        self.servo = Servo(Pins.SERVO)
+        #self.servo = Servo(Pins.SERVO)
     
     def apply_movement(self, forward_bit, breaks_bit, left_bit, right_bit):
         if forward_bit >> MovementState.FORWARD:
@@ -47,6 +47,7 @@ class manualDriver:
             gpio.output(Pins.DC_0, False)
             gpio.output(Pins.DC_0, False)
             
+        return
         if left_bit >> MovementState.LEFT:
             self.logger.debug('left')
             self.servo.min()
